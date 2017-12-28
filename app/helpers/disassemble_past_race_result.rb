0c968e7ffc406ca0609ef4544993c38ad040ca63
赤池 弘樹 <akaike@lightcafe.co.jp>
2017-12-29 05:28:03 +0900

    過去分データ作成用のデータ登録サポート　一旦コミット

--- app/helpers/disassemble_past_race_result.rb
+++ app/helpers/disassemble_past_race_result.rb
@@ -1,357 +1,359 @@
 class DisassemblePastRaceResult
   #レースキー
-  COURSE_CODE = (0..1)
-  YEAR_CODE = (2..3)
-  TIMES_CODE = 4
-  DAY_CODE = 5
-  NUMBER_OF_RACE = (6..7)
-  HORSE_NUMBER = (8..9)
+  COURSE_CODE = [0, 1]
+  YEAR_CODE = [2, 2]
+  TIMES_CODE = [4, 1]
+  DAY_CODE = [5, 1]
+  NUMBER_OF_RACE = [6, 2]
+  HORSE_NUMBER = [8, 2]
   #競争成績キー
-  RACE_PERFORMANCE_PEDIGREE_REGISTER_CODE = (10..17)
-  RACE_PERFORMANCE_DATE_YMD = (18..25)
-  RACE_PERFORMANCE_HORESE_NAME = (26..61)
+  RACE_PERFORMANCE_PEDIGREE_REGISTER_CODE = [10, 8]
+  RACE_PERFORMANCE_DATE_YMD = [18, 8]
+  RACE_PERFORMANCE_HORSE_NAME = [26, 36]
   #レース条件
-  RACE_CONDITION_DISTANCE = (62..65)
+  RACE_CONDITION_DISTANCE = [62, 4]
   #レース条件＿トラック情報
-  TRUCK_TURF_DIRT_FAULT_CODE
-  TRUCK_RIGHT_OR_LEFT
-  TRUCK_INSIDE_OR_OUTSIDE
+  TRUCK_TURF_DIRT_FAULT_CODE = [66, 1]
+  TRUCK_RIGHT_OR_LEFT = [67, 1]
+  TRUCK_INSIDE_OR_OUTSIDE = [68, 1]
   #レース条件
-  RACE_CONDITION_RIDING_GROUND_CONDITION
-  RACE_CONDITION_CLASS
-  RACE_CONDITION_REQUIREMENT
-  RACE_CONDITION_SYMBOL
-  RACE_CONDITION_WEIGHT
-  RACE_CONDITION_GRADE
-  RACE_CONDITION_WEIGHT
-  RACE_CONDITION_RACE_NAME
-  RACE_CONDITION_HORSE_COUNT
-  RACE_CONDITION_RACE_NAME_SHORT
+  RACE_CONDITION_RIDING_GROUND_CONDITION = [69, 2]
+  RACE_CONDITION_CLASS = [71, 2]
+  RACE_CONDITION_REQUIREMENT = [73, 2]
+  RACE_CONDITION_SYMBOL = [75, 3]
+  RACE_CONDITION_WEIGHT = [78, 1]
+  RACE_CONDITION_GRADE = [79, 1]
+  RACE_CONDITION_RACE_NAME = [80, 50]
+  RACE_CONDITION_HORSE_COUNT = [130, 2]
+  RACE_CONDITION_RACE_NAME_SHORT = [132, 8]
 
   #馬成績
-  HOURSE_RACE_PERFORMANCE_RANKING
-  HOURSE_RACE_PERFORMANCE_ABNORMAL_CLASSIFICATION
-  HOURSE_RACE_PERFORMANCE_TIME
-  HOURSE_RACE_PERFORMANCE_BASIS_WEIGHT
-  HOURSE_RACE_PERFORMANCE_JOCKEY_NAME
-  HOURSE_RACE_PERFORMANCE_CONFIRM_WIN_ODDS
-  HOURSE_RACE_PERFORMANCE_CONFIRM_WIN_POPULAR_RANK
+  HOURSE_RACE_PERFORMANCE_RANKING = [140, 2]
+  HOURSE_RACE_PERFORMANCE_ABNORMAL_CLASSIFICATION = [142, 1]
+  HOURSE_RACE_PERFORMANCE_TIME = [143, 4]
+  HOURSE_RACE_PERFORMANCE_BASIS_WEIGHT = [147, 3]
+  HOURSE_RACE_PERFORMANCE_JOCKEY_NAME = [150, 12]
+  HOURSE_RACE_PERFORMANCE_TRAINER_NAME = [162, 12]
+  HOURSE_RACE_PERFORMANCE_CONFIRM_WIN_ODDS = [174, 6]
+  HOURSE_RACE_PERFORMANCE_CONFIRM_WIN_POPULAR_RANK = [180, 2]
 
   #JRDBデータ
-  JRDB_IDM
-  JRDB_PRIMARY_POINT
-  JRDB_RIDING_GROUND_REFERENCE
-  JRDB_PACE
-  JRDB_LAGGING
-  JRDB_POSITION_OF_RACE
-  JRDB_UNFAVORABLE
-  JRDB_UNFAVORABLE_BEFORE
-  JRDB_UNFAVORABLE_MIDDLE
-  JRDB_UNFAVORABLE_AFTER
-  JRDB_RACE
-  JRDB_COURSE_REMOVAL
-  JRDB_RISING_CODE
-  JRDB_CLASS_CODE
-  JRDB_HORSE_BODY_CODE
-  JRDB_SIGN_CODE
-  JRDB_RACE_PACE
-  JRDB_HORSE_PACE
-  JRDB_TEN_INDEX
-  JRDB_RISING_CODE
-  JRDB_PACE_INDEX
-  JRDB_RACE_PACE_INDEX
-  JRDB_FIRST_SECOND_HORSE_NAME
-  JRDB_FIRST_SECOND_HORSE_TIME_REFERENCE
-  JRDB_BEFORE_3F_TIME
-  JRDB_AFTER_3F_TIME
-  JRDB_RESERVE
+  JRDB_IDM = [182, 3]
+  JRDB_PRIMARY_POINT = [185, 3]
+  JRDB_RIDING_GROUND_REFERENCE = [188, 3]
+  JRDB_PACE = [191, 3]
+  JRDB_LAGGING = [194, 3]
+  JRDB_POSITION_OF_RACE = [195, 3]
+  JRDB_UNFAVORABLE = [200, 3]
+  JRDB_UNFAVORABLE_BEFORE = [203, 3]
+  JRDB_UNFAVORABLE_MIDDLE = [206, 3]
+  JRDB_UNFAVORABLE_AFTER = [209, 3]
+  JRDB_RACE = [212, 3]
+  JRDB_COURSE_REMOVAL = [215, 1]
+  JRDB_RISING_CODE = [216, 1]
+  JRDB_CLASS_CODE = [217, 2]
+  JRDB_HORSE_BODY_CODE = [219, 1]
+  JRDB_SIGN_CODE = [220, 1]
+  JRDB_RACE_PACE = [221, 1]
+  JRDB_HORSE_PACE = [222, 1]
+  JRDB_TEN_INDEX = [223, 5]
+  JRDB_RISING_INDEX_CODE = [228, 5]
+  JRDB_PACE_INDEX = [233, 5]
+  JRDB_RACE_PACE_INDEX = [238, 5]
+  JRDB_FIRST_SECOND_HORSE_NAME = [243, 12]
+  JRDB_FIRST_SECOND_HORSE_TIME_REFERENCE = [255, 3]
+  JRDB_BEFORE_3F_TIME = [258, 3]
+  JRDB_AFTER_3F_TIME = [261, 3]
+  JRDB_RESERVE = [264, 24]
 
-  def initialize(pastRaceResult)
-    @pastRaceResult = pastRaceResult
+  attr_accessor :pastRaceResult
+
+  def initialize(pastRaceResultRecord)
+    @pastRaceResult = pastRaceResultRecord
   end
 
   #開催競馬場コードを取得する
   def getRaceCourseCode
-    @pastRaceResult.slice COURSE_CODE
+    @pastRaceResult.b.COURSE_CODE.force_encoding("utf-8")
   end
 
   #開催年を取得する
   def getYearCode
-    @pastRaceResult.slice YEAR_CODE
+    @pastRaceResult.b.YEAR_CODE.force_encoding("utf-8")
   end
 
   #競馬場ごとの回を取得する
   def getTimesCode
-    @pastRaceResult.slice TIMES_CODE
+    @pastRaceResult.b.TIMES_CODE.force_encoding("utf-8")
   end
 
-  #競馬場ごとの日目を取得する(16進数)
+  #競馬場ごとの日目を取得する
   def getDayCode
-    @pastRaceResult.slice TIMES_CODE
+    @pastRaceResult.b.DAY_CODE.force_encoding("utf-8")
   end
 
   #レース番号を取得する
   def getNumberOfRace
-    @pastRaceResult.slice NUMBER_OF_RACE
+    @pastRaceResult.b.NUMBER_OF_RACE.force_encoding("utf-8")
   end
 
   #馬番を取得する
   def getRacePerformanceHorseNumber
-    @pastRaceResult.slice HORSE_NUMBER
+    @pastRaceResult.b.HORSE_NUMBER.force_encoding("utf-8")
   end
 
   #血統登録番号を取得する
   def getRacePerformancePedigreeRegisterCode
-    @pastRaceResult.slice RACE_PERFORMANCE_PEDIGREE_REGISTER_CODE
+    @pastRaceResult.b.RACE_PERFORMANCE_PEDIGREE_REGISTER_CODE.force_encoding("utf-8")
   end
 
   #年月日を取得する
   def getRacePerformanceDateYmd
-    @pastRaceResult.slice RACE_PERFORMANCE_DATE_YMD
+    @pastRaceResult.b.RACE_PERFORMANCE_DATE_YMD.force_encoding("utf-8")
   end
 
   #馬名を取得する
   def getRacePerformanceHoreseName
-    @pastRaceResult.slice RACE_PERFORMANCE_HORESE_NAME
+    @pastRaceResult.b.RACE_PERFORMANCE_HORESE_NAME.force_encoding("utf-8")
   end
 
   #レース条件距離を取得する
   def getRaceConditionDistance
-    @pastRaceResult.slice RACE_CONDITION_DISTANCE
+    @pastRaceResult.b.RACE_CONDITION_DISTANCE.force_encoding("utf-8")
   end
 
   #レース条件＿トラック情報＿芝ダ障害コードを取得する
   def getTruckTurfDirtFaultCode
-    @pastRaceResult.slice TRUCK_TURF_DIRT_FAULT_CODE
+    @pastRaceResult.b.TRUCK_TURF_DIRT_FAULT_CODE.force_encoding("utf-8")
   end
 
   #レース条件＿トラック情報＿右左コードを取得する
   def getTruckRightOrLeft
-    @pastRaceResult.slice TRUCK_RIGHT_OR_LEFT
+    @pastRaceResult.b.TRUCK_RIGHT_OR_LEFT.force_encoding("utf-8")
   end
 
   #レース条件＿トラック情報＿内外を取得する
   def getTruckInsideOrOutside
-    @pastRaceResult.slice TRUCK_INSIDE_OR_OUTSIDE
+    @pastRaceResult.b.TRUCK_INSIDE_OR_OUTSIDE.force_encoding("utf-8")
   end
 
   #レース条件＿トラック情報＿馬場状態を取得する
   def getRaceConditionRidingGroundCondition
-    @pastRaceResult.slice RACE_CONDITION_RIDING_GROUND_CONDITION
+    @pastRaceResult.b.RACE_CONDITION_RIDING_GROUND_CONDITION.force_encoding("utf-8")
   end
 
   #レース条件＿トラック情報＿種別を取得する
   def getRaceConditionClass
-    @pastRaceResult.slice RACE_CONDITION_CLASS
+    @pastRaceResult.b.RACE_CONDITION_CLASS.force_encoding("utf-8")
   end
 
   #レース条件＿トラック情報＿条件を取得する
   def getRaceConditionRequirement
-    @pastRaceResult.slice RACE_CONDITION_REQUIREMENT
+    @pastRaceResult.b.RACE_CONDITION_REQUIREMENT.force_encoding("utf-8")
   end
 
   #レース条件＿トラック情報＿記号を取得する
   def getRaceConditionSymbol
-    @pastRaceResult.slice RACE_CONDITION_SYMBOL
+    @pastRaceResult.b.RACE_CONDITION_SYMBOL.force_encoding("utf-8")
   end
 
   #レース条件＿トラック情報＿重量を取得する
   def getRaceConditionWeight
-    @pastRaceResult.slice RACE_CONDITION_WEIGHT
+    @pastRaceResult.b.RACE_CONDITION_WEIGHT.force_encoding("utf-8")
   end
 
   #レース条件＿トラック情報＿グレードを取得する
   def getRaceConditionGrade
-    @pastRaceResult.slice RACE_CONDITION_GRADE
+    @pastRaceResult.b.RACE_CONDITION_GRADE.force_encoding("utf-8")
   end
 
   #レース条件＿トラック情報＿レース名を取得する
   def getRaceConditionRaceName
-    @pastRaceResult.slice RACE_CONDITION_RACE_NAME
+    @pastRaceResult.b.RACE_CONDITION_RACE_NAME.force_encoding("utf-8")
   end
 
   #レース条件＿トラック情報＿頭数を取得する
   def getRaceConditionHorseCount
-    @pastRaceResult.slice RACE_CONDITION_HORSE_COUNT
+    @pastRaceResult.b.RACE_CONDITION_HORSE_COUNT.force_encoding("utf-8")
   end
 
   #レース条件＿トラック情報＿レース名略称を取得する
   def getRaceConditionRaceNameShort
-    @pastRaceResult.slice RACE_CONDITION_RACE_NAME_SHORT
+    @pastRaceResult.b.RACE_CONDITION_RACE_NAME_SHORT.force_encoding("utf-8")
   end
 
   #馬成績＿着順を取得する
   def getHourseRacePerformanceRanking
-    @pastRaceResult.slice HOURSE_RACE_PERFORMANCE_RANKING
+    @pastRaceResult.b.HOURSE_RACE_PERFORMANCE_RANKING.force_encoding("utf-8")
   end
 
   #馬成績＿異常区分を取得する
   def getHourseRacePerformanceAbnormalClassification
-    @pastRaceResult.slice HOURSE_RACE_PERFORMANCE_ABNORMAL_CLASSIFICATION
+    @pastRaceResult.b.HOURSE_RACE_PERFORMANCE_ABNORMAL_CLASSIFICATION.force_encoding("utf-8")
   end
 
   #馬成績＿タイムを取得する
   def getHourseRacePerformanceTime
-    @pastRaceResult.slice HOURSE_RACE_PERFORMANCE_TIME
+    @pastRaceResult.b.HOURSE_RACE_PERFORMANCE_TIME.force_encoding("utf-8")
   end
 
   #馬成績＿斤量を取得する
   def getHourseRacePerformanceBasisWeight
-    @pastRaceResult.slice HOURSE_RACE_PERFORMANCE_BASIS_WEIGHT
+    @pastRaceResult.b.HOURSE_RACE_PERFORMANCE_BASIS_WEIGHT.force_encoding("utf-8")
   end
 
   #馬成績＿騎手名を取得する
   def getHourseRacePerformanceJockeyName
-    @pastRaceResult.slice HOURSE_RACE_PERFORMANCE_JOCKEY_NAME
+    @pastRaceResult.b.HOURSE_RACE_PERFORMANCE_JOCKEY_NAME.force_encoding("utf-8")
   end
 
   #馬成績＿調教師名を取得する
   def getHourseRacePerformanceTrainerName
-    @pastRaceResult.slice HOURSE_RACE_PERFORMANCE_TRAINER_NAME
+    @pastRaceResult.b.HOURSE_RACE_PERFORMANCE_TRAINER_NAME.force_encoding("utf-8")
   end
 
   #馬成績＿確定単勝オッズを取得する
   def getHourseRacePerformanceConfirmWinOdds
-    @pastRaceResult.slice HOURSE_RACE_PERFORMANCE_CONFIRM_WIN_ODDS
+    @pastRaceResult.b.HOURSE_RACE_PERFORMANCE_CONFIRM_WIN_ODDS.force_encoding("utf-8")
   end
 
   #馬成績＿確定単勝人気順位を取得する
   def getHourseRacePerformanceConfirmWinPopularRank
-    @pastRaceResult.slice HOURSE_RACE_PERFORMANCE_CONFIRM_WIN_POPULAR_RANK
+    @pastRaceResult.b.HOURSE_RACE_PERFORMANCE_CONFIRM_WIN_POPULAR_RANK.force_encoding("utf-8")
   end
 
   #JRDBデータ＿IDMを取得する
   def getJrdbIdm
-    @pastRaceResult.slice JRDB_IDM
+    @pastRaceResult.b.JRDB_IDM.force_encoding("utf-8")
   end
 
   #JRDBデータ＿素点を取得する
   def getJrdbPrimaryPoint
-    @pastRaceResult.slice JRDB_PRIMARY_POINT
+    @pastRaceResult.b.JRDB_PRIMARY_POINT.force_encoding("utf-8")
   end
 
   #JRDBデータ＿馬場差を取得する
   def getJrdbRidingGroundReference
-    @pastRaceResult.slice JRDB_RIDING_GROUND_REFERENCE
+    @pastRaceResult.b.JRDB_RIDING_GROUND_REFERENCE.force_encoding("utf-8")
   end
 
   #JRDBデータ＿ペースを取得する
   def getJrdbPace
-    @pastRaceResult.slice JRDB_PACE
+    @pastRaceResult.b.JRDB_PACE.force_encoding("utf-8")
   end
 
   #JRDBデータ＿出遅を取得する
   def getJrdbLagging
-    @pastRaceResult.slice JRDB_LAGGING
+    @pastRaceResult.b.JRDB_LAGGING.force_encoding("utf-8")
   end
 
   #JRDBデータ＿位置取を取得する
   def getJrdbPositionOfRace
-    @pastRaceResult.slice JRDB_POSITION_OF_RACE
+    @pastRaceResult.b.JRDB_POSITION_OF_RACE.force_encoding("utf-8")
   end
 
   #JRDBデータ＿不利を取得する
   def getJrdbUnfavorable
-    @pastRaceResult.slice JRDB_UNFAVORABLE
+    @pastRaceResult.b.JRDB_UNFAVORABLE.force_encoding("utf-8")
   end
 
   #JRDBデータ＿前不利を取得する
   def getJrdbUnfavorableBefore
-    @pastRaceResult.slice JRDB_UNFAVORABLE_BEFORE
+    @pastRaceResult.b.JRDB_UNFAVORABLE_BEFORE.force_encoding("utf-8")
   end
 
   #JRDBデータ＿中不利を取得する
   def getJrdbUnfavorableMiddle
-    @pastRaceResult.slice JRDB_UNFAVORABLE_MIDDLE
+    @pastRaceResult.b.JRDB_UNFAVORABLE_MIDDLE.force_encoding("utf-8")
   end
 
   #JRDBデータ＿後不利を取得する
   def getJrdbUnfavorableAfter
-    @pastRaceResult.slice JRDB_UNFAVORABLE_AFTER
+    @pastRaceResult.b.JRDB_UNFAVORABLE_AFTER.force_encoding("utf-8")
   end
 
   #JRDBデータ＿レースを取得する
   def getJrdbRace
-    @pastRaceResult.slice JRDB_RACE
+    @pastRaceResult.b.JRDB_RACE.force_encoding("utf-8")
   end
 
   #JRDBデータ＿コース取を取得する
   def getJrdbCourseRemoval
-    @pastRaceResult.slice JRDB_COURSE_REMOVAL
+    @pastRaceResult.b.JRDB_COURSE_REMOVAL.force_encoding("utf-8")
   end
 
   #JRDBデータ＿上昇度を取得する
   def getJrdbRisingCode
-    @pastRaceResult.slice JRDB_RISING_CODE
+    @pastRaceResult.b.JRDB_RISING_CODE.force_encoding("utf-8")
   end
 
   #JRDBデータ＿クラスを取得する
   def getJrdbClassCode
-    @pastRaceResult.slice JRDB_CLASS_CODE
+    @pastRaceResult.b.JRDB_CLASS_CODE.force_encoding("utf-8")
   end
 
   #JRDBデータ＿馬体を取得する
   def getJrdbHorseBodyCode
-    @pastRaceResult.slice JRDB_HORSE_BODY_CODE
+    @pastRaceResult.b.JRDB_HORSE_BODY_CODE.force_encoding("utf-8")
   end
 
   #JRDBデータ＿気配を取得する
   def getJrdbSignCode
-    @pastRaceResult.slice JRDB_SIGN_CODE
+    @pastRaceResult.b.JRDB_SIGN_CODE.force_encoding("utf-8")
   end
 
   #JRDBデータ＿レースペースを取得する
   def getJrdbRacePace
-    @pastRaceResult.slice JRDB_RACE_PACE
+    @pastRaceResult.b.JRDB_RACE_PACE.force_encoding("utf-8")
   end
 
   #JRDBデータ＿馬ペースを取得する
   def getJrdbHorsePace
-    @pastRaceResult.slice JRDB_HORSE_PACE
+    @pastRaceResult.b.JRDB_HORSE_PACE.force_encoding("utf-8")
   end
 
   #JRDBデータ＿テン指数を取得する
   def getJrdbTenIndex
-    @pastRaceResult.slice JRDB_TEN_INDEX
+    @pastRaceResult.b.JRDB_TEN_INDEX.force_encoding("utf-8")
   end
 
   #JRDBデータ＿上がり指数を取得する
   def getJrdbRisingIndexCode
-    @pastRaceResult.slice JRDB_RISING_CODE
+    @pastRaceResult.b.JRDB_RISING_CODE.force_encoding("utf-8")
   end
 
   #JRDBデータ＿ペース指数を取得する
   def getJrdbPaceIndex
-    @pastRaceResult.slice JRDB_PACE_INDEX
+    @pastRaceResult.b.JRDB_PACE_INDEX.force_encoding("utf-8")
   end
 
   #JRDBデータ＿レースＰ指数を取得する
   def getJrdbRacePaceIndex
-    @pastRaceResult.slice JRDB_RACE_PACE_INDEX
+    @pastRaceResult.b.JRDB_RACE_PACE_INDEX.force_encoding("utf-8")
   end
 
   #JRDBデータ＿1(2)着馬名を取得する
   def getJrdbFirstSecondHorseName
-    @pastRaceResult.slice JRDB_FIRST_SECOND_HORSE_NAME
+    @pastRaceResult.b.JRDB_FIRST_SECOND_HORSE_NAME.force_encoding("utf-8")
   end
 
   #JRDBデータ＿1(2)着タイム差を取得する
   def getJrdbFirstSecondHorseTimeReference
-    @pastRaceResult.slice JRDB_FIRST_SECOND_HORSE_TIME_REFERENCE
+    @pastRaceResult.b.JRDB_FIRST_SECOND_HORSE_TIME_REFERENCE.force_encoding("utf-8")
   end
 
   #JRDBデータ＿前３Ｆタイムを取得する
   def getJrdbBefore3fTime
-    @pastRaceResult.slice JRDB_BEFORE_3F_TIME
+    @pastRaceResult.b.JRDB_BEFORE_3F_TIME.force_encoding("utf-8")
   end
 
   #JRDBデータ＿後３Ｆタイムを取得する
   def getJrdbAfter3fTime
-    @pastRaceResult.slice JRDB_AFTER_3F_TIME
-  end
-  #予備
-  def getJrdbReserve
-    @pastRaceResult.slice JRDB_RESERVE
+    @pastRaceResult.b.JRDB_AFTER_3F_TIME.force_encoding("utf-8")
   end
 
+  #予備
+  def getJrdbReserve
+    @pastRaceResult.b.JRDB_RESERVE.force_encoding("utf-8")
+  end
 end
